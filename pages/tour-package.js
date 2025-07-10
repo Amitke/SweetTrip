da456bcd5a3f26c411eabd1b8c6665bd7ace3484
Amitke <sonunikki8819@gmail.com>
2025-07-10 20:17:04 +0530

    cab service content update

--- pages/tour-package.js
+++ pages/tour-package.js
@@ -41,50 +41,18 @@ export default function HotelBooking() {
       ? getTopSightSeeing.topSightSeeing?.topSightSeeing[0].topSightSeeingInfo
       : getTopSightSeeing?.error;
 
-  const topSightSeeingTitle =
-    getTopSightSeeing && getTopSightSeeing.status
-      ? getTopSightSeeing.topSightSeeing?.topSightSeeing[0].topSightSeeingTitle
-      : getTopSightSeeing?.error;
   return (
     <>
       <Head>
-      <title> Top Tour and Travel Agency in Varanasi | SweetTrip </title>
-        
-        <meta name = "description"
-        content = "Explore the best deals on Varanasi to Ayodhya tour packages, Varanasi Ayodhya Allahabad tour packages and Varanasi to Bodhgaya tour packages.Get 1000 off on all packages." />
-        < link rel = "canonical" href = "https://sweettrip.in/tour-package" />
-        <meta property="og:title" content="Top Tour and Travel Agency in Varanasi | SweetTrip"/>
-        <meta property="og:description" content="Explore the best deals on Varanasi to Ayodhya tour
-                  packages, Varanasi Ayodhya Allahabad tour packages and Varanasi to Bodhgaya tour
-                  packages. Get 1000 off on all packages." />
+        <title>Sweet Trip- Tour Packages from Varanasi with 1000 Off!</title>
+        <meta
+          name="description"
+          content="Explore the best deals on Varanasi to Ayodhya tour packages, Varanasi Ayodhya Allahabad tour packages and Varanasi to Bodhgaya tour packages. Get 1000 off on all packages."
+        />
         <meta
           name="keywords"
           content="Varanasi to Ayodhya tour package, Varanasi Ayodhya tour package, Varanasi Ayodhya Allahabad tour package, Varanasi Ayodhya Gaya tour package, Varanasi Allahabad Ayodhya tour package, Varanasi Ayodhya Prayagraj, Varanasi Ayodhya Prayagraj tour, Varanasi Ayodhya tour"
         />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://sweettrip.in/tour-package" />
-        <meta property="og:image" content="https://sweettrip.in/images/logo.svg" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Top Tour and Travel Agency in Varanasi | SweetTrip"/>
-        <meta name="twitter:description" content="Explore the best deals on Varanasi to Ayodhya tour
-        packages, Varanasi Ayodhya Allahabad tour packages and Varanasi to Bodhgaya tour
-        packages. Get 1000 off on all packages." />
-        <meta name="twitter:image" content="https://sweettrip.in/images/logo.svg" />
-            < script
-            type = "application/ld+json"
-            dangerouslySetInnerHTML = {
-              {
-                __html: JSON.stringify({
-                  "@context": "https://schema.org",
-                  "@type": "WebPage",
-                  "name": "Top Tour and Travel Agency in Varanasi | SweetTrip",
-                  "url": "https://sweettrip.in/tour-package",
-                  "description": "Explore the best deals on Varanasi to Ayodhya tour packages, Varanasi Ayodhya Allahabad tour packages and Varanasi to Bodhgaya tour packages.Get 1000 off on all packages."
-                }),
-              }
-            }
-            />
         <link rel="canonical" href="https://sweettrip.in/tour-package" />
       </Head>
       <>
@@ -93,7 +61,7 @@ export default function HotelBooking() {
           para={sectionHeader.para}
           tourPackageClass={sectionHeader}
         />
-        <TopSightSeeing topSightSeeingInfo={topSightSeeingInfo} topSightSeeingTitle={topSightSeeingTitle}/>
+        <TopSightSeeing topSightSeeingInfo={topSightSeeingInfo}/>
         <Faq faqsData={faqsData} faqsError={faqsError} />
       </>
     </>
