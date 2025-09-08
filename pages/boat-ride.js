309d0b2985e55549e7f217c837b59cd0d32f0ddb
Amitke <sonunikki8819@gmail.com>
2025-09-08 10:36:47 +0530

    boat banner

--- pages/boat-ride.js
+++ pages/boat-ride.js
@@ -98,6 +98,7 @@ export default function BoatRide() {
         <SectionHeader
           title={sectionHeader.title}
           para={sectionHeader.para}
+          boatBannerClass={sectionHeader}
         />
         <BoatHireContent />
         <div className="container mx-auto">
