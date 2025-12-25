4a85e25e6afb7098f4be69daab87bd844059e3a8
Amitke <sonunikki8819@gmail.com>
2025-12-25 17:45:50 +0530

    add car post page

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
-
+import React,{ useEffect } from "react";
+import ReactGA from "react-ga4";
 import Header from "./components/common/header/header";
 import Footer from "./components/common/footer/footer";
 import {wrapper} from "./../redux/store";
 import './../utils/utils';
 import "@/styles/globals.scss";
 
+ReactGA.initialize("G-GGF5QDQRRK"); 
+ReactGA.send("pageview");
+
 function App({ Component, pageProps }) {
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname,
+    });
+  }, []);
   return (
     <div>
       <Header />
