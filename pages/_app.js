3e0fc18a30929376022b3c0261a9445fd9ab5b48
Amitke <sonunikki8819@gmail.com>
2026-01-08 19:03:20 +0530

    add city

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import ReactGA from "react-ga4";
+import Cookies from "js-cookie";
 import Header from "./components/common/header/header";
 import Footer from "./components/common/footer/footer";
 import { wrapper } from "./../redux/store";
@@ -15,6 +16,29 @@ function App({ Component, pageProps }) {
       hitType: "pageview",
       page: window.location.pathname,
     });
+  }, []);
+
+  useEffect(() => {
+    let visitorId = Cookies.get("visitorId");
+    if (!visitorId) {
+      visitorId = Date.now() + Math.random().toString(36);
+      Cookies.set("visitorId", visitorId, { expires: 30 });
+    }
+    if (visitorId) {
+      fetch(
+        "https://script.google.com/macros/s/AKfycbx2xJM6_l_NhIqwUbqlMS4eER8Gn9-JGRnuLsb9QNEH3Inkeq4HhM3s26z_Bp14hDnRsw/exec",
+        {
+          method: "POST",
+          mode: "no-cors",
+          body: JSON.stringify({
+            visitorId: Cookies.get("visitorId"),
+            page: window.location.pathname,
+            time: new Date().toISOString(),
+            userAgent: navigator.userAgent,
+          }),
+        }
+      );
+    }
   }, []);
   return (
     <div>
